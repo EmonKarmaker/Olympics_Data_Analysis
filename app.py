2670a0e53cbdcb9ff770e56544e57130e796b369
emonkarmoker <emonkarmaker101@gmail.com>
2025-09-23 00:09:42 +0600

    statred creating medal tally

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+st.sidebar.radio(
+    'Select an option',
+    ('Medal Tally', 'Ovarall Analysis', 'Country-wise analysis','Athlete wise Analysis')
+
+)
