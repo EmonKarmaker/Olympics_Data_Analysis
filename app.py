94943eb19132555b3876425755c03d638159580b
emonkarmoker <emonkarmaker101@gmail.com>
2025-09-24 02:30:27 +0600

    break

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ df = preprocessor.preprocess(df, region_df)
 st.sidebar.title("Olympics Analysis")
 user_menu= st.sidebar.radio(
     'Select an option',
-    ('Medal Tally', 'Ovarall Analysis', 'Country-wise analysis','Athlete wise Analysis')
+    ('Medal Tally', 'Overall Analysis', 'Country-wise analysis','Athlete wise Analysis')
 
 )
-st. dataframe(df)
+
 if user_menu == 'Medal Tally':
     st.sidebar.header("Medal Tally")
     years,country=helper.country_year_list(df)
@@ -30,4 +30,34 @@ if user_menu == 'Medal Tally':
 
     if selected_year != 'Overall' and selected_country!='Overall':
         st.title(selected_country + "Performence in " + str(selected_year))
-    st.dataframe(medal_tally)
+    st.table(medal_tally)
+
+if user_menu == 'Overall Analysis':
+    editions = df['Year'].nunique() - 1
+    cities = df['City'].nunique()
+    sports = df['Sport'].nunique()
+    events = df['Event'].nunique()
+    athletes = df['Name'].nunique()
+    nations = df['region'].nunique()
+
+    # Create 3 columns
+    col1, col2, col3, col4,col5, col6 = st.columns(6)
+    with col1:
+        st.header("Editions ")
+        st.title(editions)
+    with col2:
+        st.header("Host ")
+        st.title(cities)
+    with col3:
+        st.header("Sports ")
+        st.title(sports)
+    with col4:
+        st.header("Events ")
+        st.title(events)
+    with col5:
+        st.header("Athlets ")
+        st.title(athletes)
+    with col6:
+        st.header("nations ")
+        st.title(nations)
+
