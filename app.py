f21be88c1c6f616c868dff08e6c619a42e593c48
emonkarmoker <emonkarmaker101@gmail.com>
2025-09-23 16:49:13 +0600

    break

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
+
+import preprocessor
+
+df=preprocessor.preprocess()
+
 st.sidebar.radio(
     'Select an option',
     ('Medal Tally', 'Ovarall Analysis', 'Country-wise analysis','Athlete wise Analysis')
 
 )
+st. dataframe(df)
---
+++ preprocessor.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df=pd.read_csv('athlete_events.csv')
+region_df=pd.read_csv('noc_regions.csv')
+def preprocess():
+    global df, region_df
+    #filtering for summer olympics
+    df=df[df['Season']== 'Summer']
+    #merge with region_df
+    df=df.merge(region_df,on='NOC',how='left')
+    #droping duplicated
+    df.drop_duplicates(inplace=True)
+    #one hot encoding melads
+    df = pd.concat([df, pd.get_dummies(df['Medal']).astype(int)], axis=1)
+    return df
